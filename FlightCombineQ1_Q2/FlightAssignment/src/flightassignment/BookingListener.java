c604e1451917b3a6ded62f7f0bc5cb49a2af6f71
celinezyyy <121939197+celinezyyy@users.noreply.github.com>
2023-12-31 13:39:40 +0800

    Delete FlightCombineQ1_Q2/FlightAssignment directory

--- FlightCombineQ1_Q2/FlightAssignment/src/flightassignment/BookingListener.java
+++
@@ -1,7 +0,0 @@
-package flightassignment;
-
-public interface BookingListener {
-    void onBookingMade(PassengerInfo passenger);
-    void onBookingCanceled(String passengerName, String passportNumber);
-    void onStatusUpdated(String passengerName, String passportNumber, String newStatus);
-}
